be1275e0acb568913bcaf27c866ae88c083ead41
AI-Vanilla <unconfigured@null.spigotmc.org>
2023-04-02 20:27:01 +0900

    その他のクリエイティブタブを追加

--- src/main/java/net/destinycraft/init/DestinycraftModTabs.java
+++ src/main/java/net/destinycraft/init/DestinycraftModTabs.java
@@ -20,6 +20,7 @@ public class DestinycraftModTabs {
 	public static CreativeModeTab TAB_DESTINY_CRAFT_MACHINES;
 	public static CreativeModeTab TAB_DESTINY_CRAFT_ARMORS;
 	public static CreativeModeTab TAB_DESTINY_CRAFT_SERVER_SIDE;
+	public static CreativeModeTab TAB_DESTINY_CRAFT_MISC;
 
 	public static void load() {
 		TAB_DESTINY_CRAFT_TOOLS = FabricItemGroupBuilder.create(new ResourceLocation("destinycraft", "destiny_craft_tools")).icon(() -> new ItemStack(Items.IRON_PICKAXE)).build();
@@ -29,5 +30,6 @@ public class DestinycraftModTabs {
 		TAB_DESTINY_CRAFT_MACHINES = FabricItemGroupBuilder.create(new ResourceLocation("destinycraft", "destiny_craft_machines")).icon(() -> new ItemStack(Blocks.CRAFTING_TABLE)).build();
 		TAB_DESTINY_CRAFT_ARMORS = FabricItemGroupBuilder.create(new ResourceLocation("destinycraft", "destiny_craft_armors")).icon(() -> new ItemStack(Items.IRON_CHESTPLATE)).build();
 		TAB_DESTINY_CRAFT_SERVER_SIDE = FabricItemGroupBuilder.create(new ResourceLocation("destinycraft", "destiny_craft_server_side")).icon(() -> new ItemStack(Blocks.BARRIER)).build();
+		TAB_DESTINY_CRAFT_MISC = FabricItemGroupBuilder.create(new ResourceLocation("destinycraft", "destiny_craft_misc")).icon(() -> new ItemStack(Items.GOAT_HORN)).build();
 	}
 }
